8e797912d6b46f94d961b30683b033fa806b5a08
TanvirI <Islam.Tanvir@hotmail.com>
2021-09-22 00:49:58 -0400

    adding text boxes and inputs

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import { ButtonGroup } from '@material-ui/core';
 import Button from "@material-ui/core/Button"
-import SaveIcon from "@material-ui/icons/Save"
+//import SaveIcon from "@material-ui/icons/Save"
 import Checkbox from "@material-ui/core/Checkbox"
 import FormControllabel from '@material-ui/core/FormControlLabel'
+import TextField from '@material-ui/core/TextField'
+import {makeStyles} from '@material-ui/core/styles'
+
 
 function CheckBoxExample() { 
   const[checked, setChecked] = React.useState(true)
@@ -18,23 +21,55 @@ function CheckBoxExample() {
           'aria-label' : 'secondary checkbox'
         }}
       />}
-      label = " Testing Checkbox"
+      label = " Check here because I said so :) !"
     />
 
     
      
   )
 }
+const useStyles = makeStyles ({
+  root: {
+    background: 'linear-gradient( 45deg, #333, #999)',
+    border: 0, 
+    color: 'white',
+    padding: '0 30px'
+  }
+
+
+})
+function ButtonStyled() {
+  const classes = useStyles(); 
+  return <Button className = {classes.root}>Test Styled Button </Button>
+}
+
 function App() {
   return (
 
 
     <div className="App">
       <header className="App-header">
+      <ButtonStyled/>
+
+      <TextField 
+          variant = "outlined"
+          color = "primary"
+          placeholder= "Name"
+        />
+        <TextField 
+          variant = "outlined"
+          color = "secondary"
+          type = "date"
+          placeholder = "Date of birth "
+        />
+        <TextField 
+          variant = "outlined"
+          color = "secondary"
+          type = "time"
+        />
         <CheckBoxExample />
         <ButtonGroup>
         <Button href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" variant = "contained" color = "secondary"> 
-          startIcon = {<SaveIcon />}
           Press Here 
         </Button>
 
